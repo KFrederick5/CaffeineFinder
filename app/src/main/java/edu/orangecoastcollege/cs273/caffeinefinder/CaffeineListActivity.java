b4417bcedae97b5fb5c3b55e73b728eed17d9cf6
kfrederick5 <tefsuncle@gmail.com>
2016-11-29 15:33:19 -0800

    Added a details activity

--- app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineListActivity.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.List;
 
-public class CaffeineListActivity extends AppCompatActivity {
+public class CaffeineListActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private DBHelper db;
     private List<Location> allLocationsList;
     private ListView locationsListView;
     private LocationListAdapter locationListAdapter;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +36,32 @@ public class CaffeineListActivity extends AppCompatActivity {
         locationsListView = (ListView) findViewById(R.id.locationsListView);
         locationListAdapter = new LocationListAdapter(this, R.layout.location_list_item, allLocationsList);
         locationsListView.setAdapter(locationListAdapter);
+
+        // Hookup our support map fragment to this activity
+        SupportMapFragment caffineMapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.caffineMapFragment);
+
+        caffineMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        // Use latitude and longitude for each location to create a marker on the GoogleMap
+        mMap = googleMap;
+        // Loop through each Location
+        for(Location location : allLocationsList)
+        {
+            LatLng coordinate = new LatLng(location.getLatitude(), location.getLongitude());
+            // Add a marker at that coordinate
+            mMap.addMarker(new MarkerOptions().position(coordinate).title(location.getName()));
+        }
+
+        //Change camera view to current position:
+        LatLng currentPosition = new LatLng(33.671028,-117.911305);
+        CameraPosition cameraPosition = new
+                CameraPosition.Builder().target(currentPosition).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.moveCamera(cameraUpdate);
     }
 }
