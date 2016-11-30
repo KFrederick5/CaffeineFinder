d9e2f249f69f8271de5ea2718b134b16f95380fd
kfrederick5 <tefsuncle@gmail.com>
2016-11-29 16:13:32 -0800

    Added a details activity

--- app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffineDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffineDetailsActivity.java
@@ -9,5 +9,7 @@ public class CaffineDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caffine_details);
+
+        //WORK ON THIS
     }
 }
